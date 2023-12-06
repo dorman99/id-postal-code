e963193d3a4bf5fa25f51f8d2757f6b9001c616d
Hafizh Abdillah <hafizh.abdillah@easysunday.co.id>
2023-12-07 01:15:22 +0700

    release: 1.0.0

---
+++ index.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { load, CheerioAPI } from "cheerio";
+
+export type PostalDetail = { [key: string]: string };
+
+const keys = ["province", "regency", "district", "village", "code"];
+
+export const findByPostalCode = async (
+  postalCode: number
+): Promise<Array<PostalDetail>> => {
+  const results: Array<PostalDetail> = [];
+  const url = `https://direktorikodepos.org?s=${postalCode}`;
+  const response = await axios.get(url);
+  const $ = load(response.data);
+  const tr = $("tr");
+
+  if (!!tr.length) {
+    return results;
+  }
+
+  return _mapResults($, results);
+};
+
+const _mapResults = (
+  $: CheerioAPI,
+  results: Array<PostalDetail>
+): Array<PostalDetail> => {
+  const tr = $("tr");
+  tr.each((_, row) => {
+    const td = $(row).find("td");
+    const result: PostalDetail = {};
+
+    td.each((index, col) => {
+      const value = $(col).find("a").text().trim();
+      const key = keys[index];
+      result[key] = value;
+    });
+
+    if (Object.entries(result).length > 0) {
+      results.push(result);
+    }
+  });
+  return results;
+};
